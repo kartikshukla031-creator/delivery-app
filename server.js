0d6b8499bd895c1746c65569e6eea83184645b16
Kartik Shukla <kartikshukla031@gmail.com>
2026-03-24 16:41:48 +0530

    final login fix

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ app.use("/orders", orderRoutes);
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
+  res.redirect("/login.html");
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log("Server running 🚀");
 });
